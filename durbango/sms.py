3de5f5710cc3b0da2f76906aaf1e5ff1b8fc1ac0
sshleifer <sshleifer@gmail.com>
2019-09-25 11:18:46 -0700

    cleanup

--- durbango/sms.py
+++ durbango/sms.py
@@ -5,23 +5,23 @@ import os
 # pip install twilio
 from twilio.rest import Client
 
-TWILIO_TOK, TWILIO_ID, TWILIO_NUM, RECIPIENT_NUM = (
-    os.environ['TWILIO_TOK'], os.environ['TWILIO_ID'],
-    os.environ['TWILIO_NUM'], os.environ['TWILIO_RECIPIENT_NUM']
-)
+TWILIO_TOK = os.environ['TWILIO_TOK']
+TWILIO_ID = os.environ['TWILIO_ID']
+TWILIO_NUM = os.environ['TWILIO_NUM']
+TWILIO_RECIPIENT_NUM = os.environ['TWILIO_RECIPIENT_NUM']
 
-def send_sms(message: str, to=RECIPIENT_NUM, tok=TWILIO_TOK, id=TWILIO_ID, twilio_num=RECIPIENT_NUM):
+def send_sms(message: str, to=TWILIO_RECIPIENT_NUM, tok=TWILIO_TOK, id=TWILIO_ID, twilio_num=TWILIO_NUM):
     """Send a text message after long job finishes. Texts occasionally send up to 20 minutes late.
     Args:
         message: body of message
-        to: defaults to $TWILIO_RECIPIENT_NUM
+        to: defaults to $TWILIO_RECIPIENT_NUM ( this might need a space after country code)
         tok: twilio token, defaults to $TWILIO_TOK
         id: $TWILIO_ID
         twilio_num: $TWILIO_NUM (text will be sent from this)
     """
     client = Client(id, tok)
     client.messages.create(from_=twilio_num, to=to, body=message)
-    print(f'Sent text to {RECIPIENT_NUM}')
+    print(f'Sent {message} to {to}')
 
 
 if __name__ == '__main__':
