0c227f40570a4fd8856040de96ec1841fdafc7b8
sshleifer <sshleifer@gmail.com>
2020-03-15 12:19:20 -0400

    combine diffs as late as possible

--- durbango/logging_utils.py
+++ durbango/logging_utils.py
@@ -6,6 +6,7 @@ import torch
 import psutil
 import pandas as pd
 import time
+import os
 
 
 def bytes_to_human_readable(memory_amount):
@@ -31,34 +32,56 @@ def run_gpu_mem_counter():
     py3nvml.nvmlShutdown()
     return gpu_mem
 
-def collect_log_data(msg=''):
-    process = psutil.Process(os.getpid())
-    cpu_mem = process.memory_info().rss
-    gpu_mem = run_gpu_mem_counter()
-    record = dict(cpu_mem=cpu_mem, gpu_mem=gpu_mem,
-         time = time.time(),
-         msg=msg)
-    long_msg = f'{msg}: GPU: {bytes_to_human_readable(gpu_mem)} CPU: {bytes_to_human_readable(gpu_mem)}'
-    record['long_msg'] = long_msg
-    print(long_msg)
-    return record
+
 
 class LoggingMixin:
-    def __init__(self):
-        self.logs = []
-        self.t_init = time.time()
 
-    def log_mem(self):
-        self.logs.append(collect_log_data())
+    def log_mem(self, msg='', verbose=True):
+        if not hasattr(self, 'logs'):
+            self.reset_logs()
+        self.logs.append(self.collect_log_data(msg=msg, verbose=verbose))
 
     def reset_logs(self):
         self.logs = []
+        self.t_init = time.time()
 
     @property
     def log_df(self):
+        if not hasattr(self, 'logs'):
+            self.reset_logs()
         log_df = pd.DataFrame(self.logs)
-        log_df['time'] = log_df['time'] - self.t_init
+        log_df['time_since_init'] = log_df['time'] - self.t_init
         return log_df
 
     def save_log_csv(self, path):
         self.log_df.to_csv(path)
+
+    @staticmethod
+    def collect_log_data(msg='', verbose=False):
+        process = psutil.Process(os.getpid())
+        cpu_mem = process.memory_info().rss
+        gpu_mem = run_gpu_mem_counter()
+        record = dict(cpu_mem=cpu_mem, gpu_mem=gpu_mem,
+                      time=time.time(),
+                      msg=msg)
+        long_msg = f'{msg}: GPU: {bytes_to_human_readable(gpu_mem)} CPU: {bytes_to_human_readable(gpu_mem)}'
+        record['long_msg'] = long_msg
+        if verbose:
+            print(long_msg)
+        return record
+
+    def combine_logs(self):
+        LOGS = [self.log_df]
+        def get_child_logs(module):
+            df = getattr(module, 'log_df', pd.DataFrame())
+            LOGS.append(df)
+
+        self.apply(get_child_logs)
+        log_df =  pd.concat(LOGS).sort_values('time')
+
+        return log_df.pipe(assign_diffs)
+
+def assign_diffs(log_df):
+    log_df['cpu_mem_delta'] = log_df['cpu_mem'].diff()
+    log_df['gpu_mem_delta'] = log_df['gpu_mem'].diff()
+    log_df['time_delta'] = log_df['time'].diff()
