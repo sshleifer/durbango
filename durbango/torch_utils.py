a587a5f057b28db92816165464cee867117c0f57
sshleifer <sshleifer@gmail.com>
2020-03-19 11:36:23 -0400

    Fresh new print shapes util

--- durbango/torch_utils.py
+++ durbango/torch_utils.py
@@ -1,6 +1,6 @@
 import torch
 DEFAULT_DEVICE = 'cuda:0' if torch.cuda.is_available() else 'cpu'
-
+from durbango.nb_utils import is_iterable
 
 def num_parameters(module, only_trainable: bool = False) -> int:
     """
@@ -13,3 +13,16 @@ def chunks(lst, n):
     """Yield successive n-sized chunks from lst."""
     for i in range(0, len(lst), n):
         yield lst[i:i + n]
+
+
+
+def get_shapes(x):
+    """Recursive"""
+    if hasattr(x, 'shape'):
+        return x.shape
+    elif isinstance(x, dict):
+        return {k: get_shapes(v) for k,v in x.items()}
+    elif is_iterable(x):
+        return [get_shapes(v) for v in x]
+    else:
+        return None
